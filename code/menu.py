a99c82cfb4f786e88e175085dd48eb4402ffbda6
rudney <rudpacheco@gmail.com>
2025-03-20 20:36:36 -0300

    Atualização aula 04 - inicio

--- code/menu.py
+++ code/menu.py
@@ -11,7 +11,7 @@ from Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
