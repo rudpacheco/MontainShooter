c8c443bc5e4b4042eb2f59fe348a543ca6002d7b
rudney <rudpacheco@gmail.com>
2025-03-20 21:59:53 -0300

    Atualização aula 04 - Final

--- code/enemy.py
+++ code/enemy.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-from Entity import Entity
+from Const import ENTITY_SPEED, WIN_WIDTH
+from code.Entity import Entity
 
 
 class Enemy(Entity):
-    def __init__(self):
-        pass
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
     def move(self, ):
-        pass
+        self.rect.centerx -= ENTITY_SPEED[self.name]
